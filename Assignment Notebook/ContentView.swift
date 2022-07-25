25ddf2cbf3df59c16ff8223b8d8a072acd363f97
Ela Murat <ela.murat@icloud.com>
2022-07-25 14:03:01 -0500

    Created the different courses, and edit button, and a delete option

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,10 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems =
+    [AssignmentItems(course: "Math", description: "math homework", dueDate: Date()), AssignmentItems(course: "English", description: "essay", dueDate: Date()), AssignmentItems(course: "Science", description: "at home experiment", dueDate: Date()), AssignmentItems(course: "Computer Science", description: "finish python code", dueDate: Date())]
+    @State private var showingAddItemView = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                HStack {
+                    ForEach(assignmentItems) { item in
+                        Text(item.description)
+                    }
+                    .onMove { indices, newOffset in
+                        assignmentItems.move(fromOffsets:
+                                                    indices, toOffset: newOffset)
+                    }
+                    .onDelete { indexSet in
+                        assignmentItems.remove(atOffsets:
+                                                        indexSet)
+                }
+                }
+            }
+            .sheet(isPresented: $showingAddItemView, content: {
+                AddItemView(assignmentItems: assignmentItems)
+            })
+            .navigationBarTitle("Assignment Notebook", displayMode: .inline)
+            .navigationBarItems(leading: EditButton(),
+                                trailing: Button(action: {
+                            showingAddItemView = true}) {
+                            Image(systemName: "plus")
+                })
+        }
     }
+}
+
+struct AssignmentItems: Identifiable, Codable {
+    var id = UUID ()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +53,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
