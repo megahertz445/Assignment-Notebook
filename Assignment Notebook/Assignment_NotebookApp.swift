1857c753c61f284e8c93b2d851b919c2e36937e3
Ela Murat <ela.murat@icloud.com>
2022-07-25 11:35:19 -0500

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Ela Murat on 25/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
